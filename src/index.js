9f300f7104e334087f41f038acb88b524fa389a6
Manik <manik@pixelconception.com>
2024-10-02 17:09:49 +0530

    Final code for FSDB559

--- src/index.js
+++ src/index.js
@@ -3,15 +3,31 @@ const app = express();
 const port = 3001;
 
 // mandatory route params
-app.get("/users/:role/", (req, res) => {
-  console.log("Request Method", req.params);
-  console.log("Request Method", req.query);
+app.get("/some.text", (req, res) => {
+  console.log("Request Method", req.url);
   res.send("Hello World");
 });
 
-app.get("/tasks/:user?/", (req, res) => {
-  console.log("Request Method", req.params);
-  console.log("Request Method", req.query);
+// Here the character s becomes optional
+app.get("/posts?", (req, res) => {
+  console.log("Request Method", req.url);
+  res.send("Hello World");
+});
+
+app.get("/tag*", (req, res) => {
+  console.log("Request Method", req.url);
+  res.send("Hello World");
+});
+
+//  These symbols can be used anywhere in url path in between characters or as standalone paths
+app.get("/error/*", (req, res) => {
+  console.log("Request Method", req.url);
+  res.send("Hello World");
+});
+
+//  can use regular expressions also
+app.get(/.*fly$/, (req, res) => {
+  console.log("Request Method", req.url);
   res.send("Hello World");
 });
 
