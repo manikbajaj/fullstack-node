81e6abc126c6ae8484e8810894f4072f796240b2
Manik <manik@pixelconception.com>
2024-10-02 16:15:14 +0530

    Final code for FSDB557

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-app.get("/:category", (req, res) => {
-  console.log("Request Route Params", req.params);
-  console.log("Request Query Params", req.query);
-  console.log("Request Headers", req.headers);
+app.get("/", (req, res) => {
   console.log("Request Method", req.method);
-  console.log("Request URL", req.url);
-  console.log("Request URL", req.body); // right not will return undefined
-  res.send("Hello World!");
+  res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+  console.log("Request Method", req.method);
+  res.send("Hello World");
+});
+
+app.patch("/", (req, res) => {
+  console.log("Request Method", req.method);
+  res.send("Hello World");
+});
+
+app.put("/", (req, res) => {
+  console.log("Request Method", req.method);
+  res.send("Hello World");
+});
+
+app.delete("/", (req, res) => {
+  console.log("Request Method", req.method);
+  res.send("Hello World");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
