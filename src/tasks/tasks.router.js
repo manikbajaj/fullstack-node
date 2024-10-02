6797c3242b1b1326a8c244af3316c7958545b15b
Manik <manik@pixelconception.com>
2024-10-02 20:01:25 +0530

    Code For FSDB572

--- src/tasks/tasks.router.js
+++ src/tasks/tasks.router.js
@@ -4,7 +4,10 @@ import { Router } from "express";
 export const TasksRouter = Router();
 
 // Get All Tasks
-TasksRouter.get("/tasks", (req, res) => res.send("All Tasks"));
+TasksRouter.get("/tasks", (req, res) => {
+  console.log(req.info);
+  return res.send({ data: "tasks" });
+});
 
 // POST Create a task
 TasksRouter.post("/tasks", (req, res) => res.send("Create a new task"));
