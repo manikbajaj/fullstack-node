bf01efae5b79405bdcf3edbb1594c34639762995
Manik <manik@pixelconception.com>
2024-10-02 16:03:15 +0530

    Final code for FSDB556

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-app.get("/", (req, res) => {
-  console.log(req);
+app.get("/:category", (req, res) => {
+  console.log("Request Route Params", req.params);
+  console.log("Request Query Params", req.query);
+  console.log("Request Headers", req.headers);
+  console.log("Request Method", req.method);
+  console.log("Request URL", req.url);
+  console.log("Request URL", req.body); // right not will return undefined
   res.send("Hello World!");
 });
 
