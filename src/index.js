bf716d074d2bab7a0d2f769efdd1c3ee77f1a3e5
Manik <manik@pixelconception.com>
2024-10-04 11:34:14 +0530

    Updating the branch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { TasksRouter } from "./tasks/tasks.router.js";
-import express from "express";
+const express = require("express");
+const tasksRouter = require("./tasks/tasks.router.js");
 
 const app = express();
 const port = 3001;
@@ -7,8 +7,20 @@ const port = 3001;
 //  Parsing request body
 app.use(express.json());
 
+//  Creating a middleware
+const myMiddleware = function (req, res, next) {
+  // On request
+  req.info = { appname: "Tasks Manager", author: "Cloudaffle" };
+
+  // Do not return from the middleware
+  next();
+};
+
+//  Using the middleware
+app.use(myMiddleware);
+
 //  Defining Routes
-app.use("/", TasksRouter);
+app.use("/", tasksRouter);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
--- src/tasks/tasks.router.js
+++ src/tasks/tasks.router.js
@@ -1,18 +1,21 @@
-import { Router } from "express";
+const express = require("express");
 
 /*Fire the router function*/
-export const TasksRouter = Router();
+const tasksRouter = express.Router();
 
 // Get All Tasks
-TasksRouter.get("/tasks", (req, res) => {
+tasksRouter.get("/tasks", (req, res) => {
   return res.send("Hello World");
 });
 
 // POST Create a task
-TasksRouter.post("/tasks", (req, res) => {
+tasksRouter.post("/tasks", (req, res) => {
   // Body will be undefined without middleware
   console.log(req.body);
   // You get a JavaScript object
   console.log(typeof req.body);
   res.send("Create a new task");
 });
+
+// export the task router
+module.exports = tasksRouter;
