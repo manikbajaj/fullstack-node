a6ddd515019bbafd44ded4e654fe417ceb0e162f
Manik <manik@pixelconception.com>
2024-10-04 13:11:55 +0530

    Final code for FSDB573

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import { TasksRouter } from "./tasks/tasks.router.js";
-import express from "express";
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const morgan = require("morgan");
+const tasksRouter = require("./tasks/tasks.router.js");
 
 const app = express();
 const port = 3001;
@@ -7,8 +10,28 @@ const port = 3001;
 //  Parsing request body
 app.use(express.json());
 
+// Using Morgan for logging
+/**
+ * File System Module (fs): Used to create a writable stream for the file where logs will be saved.
+ * Path Module (path): Helps with handling file paths.
+ * Create Write Stream: fs.createWriteStream() is used to create a stream that Morgan will write log data to. The 'a' flag ensures that logs are appended to the file (access.log) instead of overwriting it.
+ */
+
+var accessLogStream = fs.createWriteStream(
+  //! The 'path.join' method joins all given path segments together using the platform-specific separator
+  // '__dirname' is a Node.js variable that gives the directory name of the current module
+  // '..' moves up one directory level from the current directory
+  // 'access.log' is the file name we want to access or create in the parent directory
+  // ! File is igniored in gitignore by default and need not be sent to git
+  path.join(__dirname, "..", "access.log"),
+  {
+    flags: "a",
+  }
+);
+app.use(morgan("combined", { stream: accessLogStream }));
+
 //  Defining Routes
-app.use("/", TasksRouter);
+app.use("/", tasksRouter);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
