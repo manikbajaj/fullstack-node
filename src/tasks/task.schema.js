d1e1b4462b139a88ffce0d70e391b55f502170e9
Manik <manik@pixelconception.com>
2024-10-05 17:15:06 +0530

    Final code for FSDB626

--- src/tasks/task.schema.js
+++ src/tasks/task.schema.js
@@ -1,37 +1,45 @@
-const { Schema } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const taskSchema = new Schema({
-  title: {
-    type: String,
-    //! can also be required: true without message
-    required: [true, "Task title is required"],
-    trim: true,
-    maxlength: [100, "Title cannot be more than 100 characters"],
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      //! can also be required: true without message
+      required: [true, "Task title is required"],
+      trim: true,
+      maxlength: [100, "Title cannot be more than 100 characters"],
+    },
+    description: {
+      type: String,
+      required: [true, "Task description is required"],
+      trim: true,
+      maxlength: [500, "Description cannot be more than 500 characters"],
+    },
+    status: {
+      type: String,
+      required: [true, "Task status is required"],
+      enum: ["todo", "inProgress", "completed"],
+      default: "todo",
+    },
+    priority: {
+      type: String,
+      required: [true, "Task priority is required"],
+      enum: ["low", "normal", "high"],
+      default: "normal",
+    },
+    // Removing createdAt and adding a timestamp instead of that
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+    dueDate: {
+      type: Date,
+      required: [true, "Due date is required"],
+    },
   },
-  description: {
-    type: String,
-    required: [true, "Task description is required"],
-    trim: true,
-    maxlength: [500, "Description cannot be more than 500 characters"],
-  },
-  status: {
-    type: String,
-    required: [true, "Task status is required"],
-    enum: ["pending", "inProgress", "completed"],
-    default: "pending",
-  },
-  priority: {
-    type: String,
-    required: [true, "Task priority is required"],
-    enum: ["low", "medium", "high"],
-    default: "medium",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  dueDate: {
-    type: Date,
-    required: [true, "Due date is required"],
-  },
-});
+  { timestamps: true }
+);
+
+// Create the model and export it
+const Task = model("Task", taskSchema);
+module.exports = Task;
--- src/tasks/tasks.controller.js
+++ src/tasks/tasks.controller.js
@@ -1,4 +1,5 @@
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const Task = require("./task.schema.js");
 
 function handleGetTasks(req, res) {
   let response = [
@@ -24,8 +25,20 @@ function handleGetTasks(req, res) {
   res.status(StatusCodes.OK).json(response);
 }
 
-function handlePostTasks(req, res) {
-  res.send("POST tasks controller");
+// Convert the function to Async function
+async function handlePostTasks(req, res) {
+  const task = new Task({
+    title: req.body.title,
+    description: req.body.description,
+    status: req.body.status,
+    priority: req.body.priority,
+    dueDate: req.body.dueDate,
+  });
+
+  // Insert the article in  MongoDB database
+  await task.save();
+
+  res.json(task);
 }
 
 function handlePatchTasks(req, res) {
