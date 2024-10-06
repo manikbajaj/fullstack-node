e416dd729b902010310186b561388c84e44f60f5
Manik <manik@pixelconception.com>
2024-10-06 18:27:32 +0530

    Updating branch

--- src/tasks/tasks.router.js
+++ src/tasks/tasks.router.js
@@ -10,17 +10,27 @@ const tasksRouter = express.Router();
 tasksRouter.get("/tasks", tasksController.handleGetTasks);
 
 // POST Create a task
-tasksRouter.post("/tasks", body("title").notEmpty(), (req, res) => {
-  const result = validationResult(req);
-  //! See what happens when validation fails
-  console.log(result);
-  //! Add a condition to stop request if there are errors
-  if (result.isEmpty()) {
-    return tasksController.handlePostTasks(req, res);
-  } else {
-    res.status(StatusCodes.BAD_REQUEST).json(result.array());
+tasksRouter.post(
+  "/tasks",
+  [
+    body("title", "The title cannot be blank").notEmpty(),
+    body("title", "The title must be a string value").isString(),
+    body("dueDate", "dueDate need to be valid ISO8601 date string")
+      .notEmpty()
+      .isISO8601(),
+  ],
+  (req, res) => {
+    const result = validationResult(req);
+    //! See what happens when validation fails
+    console.log(result);
+    //! Add a condition to stop request if there are errors
+    if (result.isEmpty()) {
+      return tasksController.handlePostTasks(req, res);
+    } else {
+      res.status(StatusCodes.BAD_REQUEST).json(result.array());
+    }
   }
-});
+);
 
 // Get All Tasks
 tasksRouter.patch("/tasks", tasksController.handlePatchTasks);
