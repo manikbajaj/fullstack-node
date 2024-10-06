c5cd86be227fe15aefb2a661029ded3471cc9237
Manik <manik@pixelconception.com>
2024-10-06 19:29:17 +0530

    Final code for FSDB645

--- src/tasks/providers/createTask.provider.js
+++ src/tasks/providers/createTask.provider.js
@@ -1,13 +1,11 @@
 const Task = require("../task.schema.js");
+const { matchedData } = require("express-validator");
 
 async function createTaskProvider(req, res) {
-  const task = new Task({
-    title: req.body.title,
-    description: req.body.description,
-    status: req.body.status,
-    priority: req.body.priority,
-    dueDate: req.body.dueDate,
-  });
+  const validatedData = matchedData(req);
+  //! matchedData(req) will get the validated and sanitized data and remove extra and unwanted fields from the incoming request as well.
+  console.log(validatedData);
+  const task = new Task(validatedData);
 
   // Insert the article in  MongoDB database
   return await task.save();
