736df0ec3bbd054cba5a00f4c38d8e27b523c709
Manik <manik@pixelconception.com>
2024-10-07 14:53:10 +0530

    Final code for FSDB650

--- src/tasks/providers/createTask.provider.js
+++ src/tasks/providers/createTask.provider.js
@@ -1,7 +1,7 @@
 const Task = require("../task.schema.js");
 const { matchedData } = require("express-validator");
 const { StatusCodes } = require("http-status-codes");
-const logger = require("../../helpers/winston.helper.js");
+const errorLogger = require("../../helpers/errorLogger.helper.js");
 
 async function createTaskProvider(req, res) {
   const validatedData = matchedData(req);
@@ -11,18 +11,7 @@ async function createTaskProvider(req, res) {
     await task.save();
     return res.status(StatusCodes.CREATED).json(task);
   } catch (error) {
-    logger.error(`Error while creating task: ${error.message}`, {
-      // Manually log the error
-      metadata: {
-        // You can add additional metadata if necessary
-        //  These are logged to the error.log
-        statusCode: StatusCodes.GATEWAY_TIMEOUT,
-        method: req.method,
-        url: req.originalUrl,
-        body: validatedData,
-        completeError: error,
-      },
-    });
+    errorLogger("Error while creating task: ", req, error);
     return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
       reason: "Unable to process your request at the moment, please try later.",
     });
