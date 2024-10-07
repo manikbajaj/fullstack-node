3843d0b53c95d854e62e731c3b7f509d0b17d494
Manik <manik@pixelconception.com>
2024-10-07 17:08:56 +0530

    Final code for FSDB651

--- src/tasks/providers/getTasks.provider.js
+++ src/tasks/providers/getTasks.provider.js
@@ -1,7 +1,15 @@
 const Task = require("../task.schema.js");
+const { matchedData } = require("express-validator");
+const { StatusCodes } = require("http-status-codes");
 
 async function getTasksProvider(req, res) {
-  return await Task.find();
+  // ! Since query params are not required we only need santized data and set the defaul values
+  const data = matchedData(req);
+
+  console.log(data);
+  const tasks = await Task.find();
+
+  return res.status(StatusCodes.OK).json(tasks);
 }
 
 module.exports = getTasksProvider;
--- src/tasks/tasks.router.js
+++ src/tasks/tasks.router.js
@@ -3,12 +3,20 @@ const tasksController = require("./tasks.controller.js");
 const { StatusCodes } = require("http-status-codes");
 const { validationResult } = require("express-validator");
 const createTaskValidator = require("./validators/createTask.validator.js");
+const getTasksValidator = require("./validators/getTasks.validator.js");
 
 /*Fire the router function*/
 const tasksRouter = express.Router();
 
 // Get All Tasks
-tasksRouter.get("/tasks", tasksController.handleGetTasks);
+tasksRouter.get("/tasks", getTasksValidator, (req, res) => {
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    return tasksController.handleGetTasks(req, res);
+  } else {
+    res.status(StatusCodes.BAD_REQUEST).json(result.array());
+  }
+});
 
 // POST Create a task
 tasksRouter.post("/tasks", createTaskValidator, (req, res) => {
