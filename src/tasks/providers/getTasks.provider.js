23a3f85fce91e781fec650d6a1886235277595e3
Manik <manik@pixelconception.com>
2024-10-08 16:36:15 +0530

    Final code for FSDB673

--- src/tasks/providers/getTasks.provider.js
+++ src/tasks/providers/getTasks.provider.js
@@ -14,7 +14,45 @@ async function getTasksProvider(req, res) {
     req.originalUrl.split("?")[0]
   }`;
 
-  const tasks = await Task.find().limit().skip();
+  // currentPage
+  const currentPage = data.page;
+  // limit
+  const limit = data.limit;
+  // sorting order
+  const order = data.order;
+  // Total pages
+  const totalPages = Math.ceil(totalTasks / limit);
+  // Next Page
+  const nextPage = currentPage === totalPages ? currentPage : currentPage + 1;
+  // Previous Page
+  const previousPage = currentPage === 1 ? currentPage : currentPage - 1;
+
+  // Fetch Data
+  const tasks = await Task.find()
+    .limit(limit)
+    .skip(currentPage - 1);
+
+  let finalResponse = {
+    data: tasks,
+    pagination: {
+      meta: {
+        itemsPerPage: limit,
+        totalItems: totalTasks,
+        currentPage: currentPage,
+        totalPages: totalPages,
+      },
+      links: {
+        first: `${baseUrl}/?limit=${limit}&page=${1}&order=${order}`,
+        last: `${baseUrl}/?limit=${limit}&page=${totalPages}&order=${order}`,
+        current: `${baseUrl}/?limit=${limit}&page=${currentPage}&order=${order}`,
+        next: `${baseUrl}/?limit=${limit}&page=${nextPage}&order=${order}`,
+        previous: `${baseUrl}/?limit=${limit}&page=${previousPage}&order=${order}`,
+      },
+    },
+  };
+
+  return res.status(StatusCodes.OK).json(finalResponse);
+
   try {
     // Insert the article in  MongoDB database
     const tasks = await Task.find();
