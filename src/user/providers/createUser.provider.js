bd472aab957584f9db4402896f8bd622dc264fdf
Manik <manik@pixelconception.com>
2024-10-08 11:54:35 +0530

    Final code for FSDB654

--- src/user/providers/createUser.provider.js
+++ src/user/providers/createUser.provider.js
@@ -1,13 +1,27 @@
 const User = require("../user.schema.js");
+const { matchedData } = require("express-validator");
+const { StatusCodes } = require("http-status-codes");
+const errorLogger = require("../../helpers/errorLogger.helper.js");
 
 async function createUserProvider(req, res) {
-  const user = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  return await user.save();
+  const validatedData = matchedData(req);
+
+  try {
+    const user = new User({
+      firstName: validatedData.firstName,
+      lastName: validatedData.lastName,
+      email: validatedData.email,
+      password: validatedData.password,
+    });
+    await user.save();
+    delete user.password;
+    return res.status(StatusCodes.OK).json(user);
+  } catch (error) {
+    errorLogger("Error while creating user: ", req, error);
+    return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
+      reason: "Unable to process your request at the moment, please try later.",
+    });
+  }
 }
 
 module.exports = createUserProvider;
--- src/user/user.router.js
+++ src/user/user.router.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const { StatusCodes } = require("http-status-codes");
+const { validationResult } = require("express-validator");
 const userController = require("./user.controller.js");
+const createUserValidator = require("./validators/createUser.validator.js");
 
 const userRouter = express.Router();
 
-userRouter.post("/create", userController.handleCreateUser);
+userRouter.post("/create", createUserValidator, (req, res) => {
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    return userController.handleCreateUser(req, res);
+  } else {
+    res.status(StatusCodes.BAD_REQUEST).json(result.array());
+  }
+});
 
 module.exports = userRouter;
