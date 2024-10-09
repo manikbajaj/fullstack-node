d89a93d8e786783fc441438dbe56cd108e9be59e
Manik <manik@pixelconception.com>
2024-10-09 14:20:09 +0530

    Final code for FSDB686

--- src/auth/auth.router.js
+++ src/auth/auth.router.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const { validationResult } = require("express-validator");
 const authController = require("./auth.controller.js");
+const loginValidator = require("./validators/login.validator.js");
 
 const authRouter = express.Router();
 
-authRouter.post("/login", authController.handleLogin);
+authRouter.post("/login", loginValidator, (req, res) => {
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    return authController.handleLogin(req, res);
+  } else {
+    res.status(StatusCodes.BAD_REQUEST).json(result.array());
+  }
+});
 
 module.exports = authRouter;
---
+++ src/auth/providers/login.provider.js
@@ -0,0 +1,16 @@
+const { matchedData } = require("express-validator");
+const { StatusCodes } = require("http-status-codes");
+
+async function loginProvider(req, res) {
+  const validatedData = matchedData(req);
+  try {
+    return res.status(StatusCodes.OK).json({});
+  } catch (error) {
+    errorLogger("Error while trying to login: ", req, error);
+    return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
+      reason: "Unable to process your request at the moment, please try later.",
+    });
+  }
+}
+
+module.exports = loginProvider;
