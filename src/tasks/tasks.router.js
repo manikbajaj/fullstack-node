8abea07b08403a64389b5ab11592fee35ea47f80
Manik <manik@pixelconception.com>
2024-10-10 14:56:02 +0530

    Final code for FSDB706

--- src/tasks/tasks.router.js
+++ src/tasks/tasks.router.js
@@ -140,7 +140,48 @@ tasksRouter.post(
   }
 );
 
-// Get All Tasks
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * /tasks:
+ *   patch:
+ *     summary: Update a task
+ *     tags: [Tasks]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/TaskUpdate'
+ *     responses:
+ *       200:
+ *         description: Shape of task response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/TaskUpdate'
+ *       401:
+ *         description: Not Authorized error
+ *         content:
+ *           application/json:
+ *             example:
+ *               error:
+ *                 message: "You are not Authorized to perform this request"
+ *       403:
+ *         description: Forbidden
+ *         content:
+ *           application/json:
+ *             example:
+ *               error:
+ *                 message: "Your token is either expired or invalid."
+ */
 tasksRouter.patch(
   "/tasks",
   [updateTaskValidator, authenticateToken],
