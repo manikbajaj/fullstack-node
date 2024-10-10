2f27b77d5214a9a126d33e3f7e30d86d0c13cdea
Manik <manik@pixelconception.com>
2024-10-10 16:53:03 +0530

    Complete code for FSDB709

--- src/auth/auth.router.js
+++ src/auth/auth.router.js
@@ -6,6 +6,29 @@ const loginValidator = require("./validators/login.validator.js");
 
 const authRouter = express.Router();
 
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: User Login
+ *     tags: [Authentication]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Login'
+ *     responses:
+ *       201:
+ *         description: Shape of task response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Login'
+ */
+
 authRouter.post("/login", loginValidator, (req, res) => {
   const result = validationResult(req);
   if (result.isEmpty()) {
@@ -16,3 +39,24 @@ authRouter.post("/login", loginValidator, (req, res) => {
 });
 
 module.exports = authRouter;
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Login:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *           description: A valid email address
+ *         password:
+ *           type: string
+ *           description: Must be atleast 8 chars and contain a number, Capital letter and a special character
+ *       example:
+ *         email: john@doe.com
+ *         password: Password123#
+ */
